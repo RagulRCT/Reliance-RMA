e347f5c4a2771a28cfadf5c796473c65811c92f5
RagulRCT <107754208+RagulRCT@users.noreply.github.com>
2022-07-25 18:36:36 +0530

    Update Posts.jsx

--- src/components/blog/Posts.jsx
+++ src/components/blog/Posts.jsx
@@ -17,10 +17,7 @@ function Posts() {
             <div class="col-lg-5">
               <h1 class="font-weight-light">This is a post title</h1>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                Personalized service IN freight Forwarding!!!
               </p>
             </div>
           </div>
